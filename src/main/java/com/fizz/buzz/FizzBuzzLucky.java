7c435ae0c969d91feb657e1af460136c554c0eae
im85288 <ianmalcolmmclaughlin@gmail.com>
2017-09-05 21:28:54 +0100

    Step 2 - Test passes and implementation logic

--- src/main/java/com/fizz/buzz/FizzBuzzLucky.java
+++ src/main/java/com/fizz/buzz/FizzBuzzLucky.java
@@ -1,16 +1,38 @@
 package com.fizz.buzz;
 
+import java.io.Serializable;
+
 /**
+ * This class extends the FizzBuzz class by replacing any number that
+ * contains a 3 with the word lucky
+ *
  * Created by ianmclaughlin on 05/09/2017.
  */
 public class FizzBuzzLucky extends FizzBuzz {
 
+    private static final int lucky = 3;
+
+    public static final String LUCKY_VALUE = "lucky";
+
+    /**
+     * Create an instance of FizzBuzzLucky with start
+     * and end range
+     *
+     * @param startRange
+     * @param endRange
+     */
     public FizzBuzzLucky(int startRange, int endRange) {
         super(startRange, endRange);
     }
 
     @Override
-    public String getParsedOutput() {
-        return "1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz";
+    protected Serializable getValue(int i) {
+        return String.valueOf(i).contains(String.valueOf(lucky)) ? LUCKY_VALUE : super.getValue(i);
+    }
+
+    public static void main(String[] args)
+    {
+        FizzBuzzLucky fizzBuzzLucky = new FizzBuzzLucky(1,20);
+        System.out.println(fizzBuzzLucky.getParsedOutput());
     }
 }
